dd63e5a18c07689f6f1ee2c4be7b094070ce4ae4
Marcus Baptiste <marbap45@gmail.com>
2015-01-15 20:17:09 -0400

    Added jQuery
    Added JQuery plugin file (didn't realize I still needed it along with
    jquery mobile). Tested and working.

--- scripts/main.js
+++ scripts/main.js
@@ -1,94 +1,95 @@
 // Titulo de Proyecto: Programaquina y los Secretos del Planeta Algoritmus;
 // Autor: Marcus Baptiste - ID: R0032401;
 
-
-//game objects
-function crearObjetosJuego(xPos, yPos, tipo) {
-	//these objects will only store information on the game object. The game engine
-	//will be responsible for rendering the visual element
-	return {
-		"xPos": xPos,
-		"yPos": yPos,
-		"tipo": tipo
-	};
-
-}
-
-
-//game levels
-function crearNivel(id, titulo) {
-	//create level object
-	return {
-		"id": id,
-		"titulo": titulo,
-		"obstaculos": [ ],
-		"herramientas": [],
-		"meta": { },
-		"huecos": [ ],
-		"entrada": { },
-		"codigoPermitido": [ ],
-		"guias": [ ]
-	};
-}
-
-
-//code-frags (fragmentos de codigo) 
-function crearCodeFrag(nombre, tipo, posicion, numero) {
-	var codeFrag = {
-		"nombre": nombre,
-		"tipo": tipo,
-		"posicion": posicion
-	};
-
-	if (tipo === 'ciclo') {
-		//make code frag object for loop
-
-	} else if (tipo === 'condInicio') {
-		//make code frag object for start condition 
-
-	} else if (tipo === 'condElse') {
-		//make code frag object for else condition
-
-	} else if (tipo === 'function') {
-		//make code frag object for function
-
-	} else {
-		//make code frag object for any other instruction
+$(document).ready(function() {
+	//game objects
+	function crearObjetosJuego(xPos, yPos, tipo) {
+		//these objects will only store information on the game object. The game engine
+		//will be responsible for rendering the visual element
+		return {
+			"xPos": xPos,
+			"yPos": yPos,
+			"tipo": tipo
+		};
 
 	}
 
-	return codeFrag;
-}
 
-//characters 
-var avatar = {
-	"nombre": "P-1",
-	"xPos": 0,
-	"yPos": 0,
-	"grafico": "camnio al grafico",
-	"tema": "default",
-	"animacion": {
-		"caer": [],
-		"volar": [],
-		"paso": [],
-
+	//game levels
+	function crearNivel(id, titulo) {
+		//create level object
+		return {
+			"id": id,
+			"titulo": titulo,
+			"obstaculos": [ ],
+			"herramientas": [],
+			"meta": { },
+			"huecos": [ ],
+			"entrada": { },
+			"codigoPermitido": [ ],
+			"guias": [ ]
+		};
 	}
-};
 
-var guia = {
-	"nombre": "C-bot",
-	"grafico": "file name of graphic",
-	"animacion": {
-		"hablar": [],
-		"saltar": [],
-		"senalar": []
+
+	//code-frags (fragmentos de codigo) 
+	function crearCodeFrag(nombre, tipo, posicion, numero) {
+		var codeFrag = {
+			"nombre": nombre,
+			"tipo": tipo,
+			"posicion": posicion
+		};
+
+		if (tipo === 'ciclo') {
+			//make code frag object for loop
+
+		} else if (tipo === 'condInicio') {
+			//make code frag object for start condition 
+
+		} else if (tipo === 'condElse') {
+			//make code frag object for else condition
+
+		} else if (tipo === 'function') {
+			//make code frag object for function
+
+		} else {
+			//make code frag object for any other instruction
+
+		}
+
+		return codeFrag;
 	}
-};
+
+	//characters 
+	var avatar = {
+		"nombre": "P-1",
+		"xPos": 0,
+		"yPos": 0,
+		"grafico": "camnio al grafico",
+		"tema": "default",
+		"animacion": {
+			"caer": [],
+			"volar": [],
+			"paso": [],
+
+		}
+	};
+
+	var guia = {
+		"nombre": "C-bot",
+		"grafico": "file name of graphic",
+		"animacion": {
+			"hablar": [],
+			"saltar": [],
+			"senalar": []
+		}
+	};
 
 
 
-//game engine
-var elJuego = {
-};
+	//game engine
+	var elJuego = {
+	};
 
-
+	alert("test");
+});
